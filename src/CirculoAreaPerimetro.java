3d0db727b064a9b84d7c315f1103aefeab5d76f4
rdsalvesPUC <s.rodrigo5@pucpr.edu.br>
2024-02-23 18:29:04 -0300

    First exercises

---
+++ src/CirculoAreaPerimetro.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CirculoAreaPerimetro {
+    public static void main(String[] args)
+    {
+        // entrada de dados
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Informe o raio do circulo: ");
+        double raio = keyboard.nextDouble();
+
+        // processamento
+        double area = raio *
+
+        // saida de dados
+        System.out.println("Área do circulo:" + area);
+        System.out.println("Perimetro do circulo:" + perimetro);
+    }
+}
---
+++ src/variaveisTiposNumericos.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class variaveisTiposNumericos {
+    public static void main(String[] args)
+    {
+//        short k = 32767;
+//        System.out.println(k);
+//        k++;
+//        System.out.println(k);
+
+//        int r = 2;
+//        int t = 5;
+//        int a = r * 7 - t;
+//        System.out.println(a);
+//        int b = r * (7 - t);
+//        System.out.println(b);
+//        int k = 10;
+//        k = k + ( t / 2 );
+//        System.out.println(k);
+
+//        int z = 50000;
+//        short r = (short) z;
+//        System.out.println(r);
+
+//        float f = 3.5f;
+//        double d = 8.0;
+//        double e = f * d;
+//        System.out.println(e);
+
+//        float v = 7.5f;
+//        System.out.println(v);
+//        double w = v;
+//        System.out.println(w);
+
+//        double y = 1E+40;
+//        System.out.println(y);
+//        float x = (float)y;
+//        System.out.println(x);
+
+//        int m = (int)8.6;
+//        System.out.println(m);
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite a sua idade: ");
+        int k = teclado.nextInt();
+        System.out.println("Você tem " + k + " anos");
+    }
+}
+
