f9abb713781ba718bf196ffcdf0b8f71ffd96933
rdsalvesPUC <s.rodrigo5@pucpr.edu.br>
2024-03-07 00:28:16 -0300

    Projetil.java foi resolvido

--- src/Projetil.java
+++ src/Projetil.java
@@ -9,27 +9,26 @@ public class Projetil {
         System.out.println("Informa a distancia: ");
         int R = teclado.nextInt();
 
-        double g = 9.8f;
+        double radiano_alpha = Math.toRadians(alpha);
+        double g = 9.8;
 
 //        Velocidade inicial: V 0 = √( (R g) / (2 sen α cos α) ), onde g = 9.8 m/s2
-//        double VI = Math.sqrt((R * g) / (2 * Math.sin(alpha) * Math.cos(alpha)));
-//        double VI = Math.sqrt(R * g);
-//        double VI = (R * g) / (2 * Math.sin(alpha) * Math.cos(alpha));
-        double VI = Math.sin(alpha);
-        System.out.println(VI);
+        double VI = Math.sqrt((R * g) / (2 * Math.sin(radiano_alpha) * Math.cos(radiano_alpha)));
 
-////        Tempo de lançamento: T = (2 V0 sen α)/g
-//        double TL = (2 * VI * Math.sin(alpha)) / g;
-//
-////        Coordenadas
-//        double x = (VI * Math.cos(alpha)) * TL;
-//        double y = (VI * Math.sin(alpha)) * TL - (g * Math.pow(TL, 2) / 2);
-//
-//
-//        System.out.println("Velocidade Inicial: " + VI + "m/s");
-//        System.out.println("Tempo Total: " + TL + "s");
-//
-//        System.out.print(x);
-//        System.out.print(y);
+//        Tempo de lançamento: T = (2 V0 sen α)/g
+        double TL = (2 * VI * Math.sin(radiano_alpha)) / g;
+
+//        Coordenadas
+//        while () {
+            double x = (VI * Math.cos(radiano_alpha)) * TL;
+            double y = (VI * Math.sin(radiano_alpha)) * TL - (g * Math.pow(TL, 2) / 2);
+
+
+            System.out.println("Velocidade Inicial: " + VI + "m/s");
+            System.out.println("Tempo Total: " + TL + "s");
+
+            System.out.print(x);
+            System.out.print(y);
+        }
     }
-}
+//}
